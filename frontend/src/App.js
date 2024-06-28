70e16c1b5d3f3292863d12d2b5138b16afe64e95
Pranjali bramhankar <pranjalibramhankar14@gmail.com>
2024-06-29 00:02:30 +0530

    added documentatin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,33 @@
 import './App.css'
 
 import { useState, useEffect } from 'react'
-import {io} from 'socket.io-client'
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+import { io } from 'socket.io-client'
+import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth'
 
 const socket = io.connect('http://localhost:5000')
 
 function App() {
-
   const firebaseConfig = {
-    apiKey: "AIzaSyChDBuL-_57SE5tLILhtMkpiLyVGN5LvMY",
-    authDomain: "chat-app-pranjali.firebaseapp.com",
-    projectId: "chat-app-pranjali",
-    storageBucket: "chat-app-pranjali.appspot.com",
-    messagingSenderId: "51859414044",
-    appId: "1:51859414044:web:52732dab73dbd339f5d3d9"
-  };
+    apiKey: 'AIzaSyDxGfri2DwfmMCNOf_l1f0JL-sXqiOsjYE',
+    authDomain: 'chat-room-356b7.firebaseapp.com',
+    projectId: 'chat-room-356b7',
+    storageBucket: 'chat-room-356b7.appspot.com',
+    messagingSenderId: '998194300150',
+    appId: '1:998194300150:web:427ebdd92d27d6d87b18d7',
+  }
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  getAuth(app);
-  const [currentUser, setCurrentUser] = useState();
-  const provider = new GoogleAuthProvider();
+  const app = initializeApp(firebaseConfig)
+  getAuth(app)
+  const [currentUser, setCurrentUser] = useState()
+  const provider = new GoogleAuthProvider()
 
   const [message, setMessage] = useState('')
   const [chat, setChat] = useState([])
@@ -35,9 +40,11 @@ function App() {
 
   useEffect(() => {
     onAuthStateChanged(getAuth(), (user) => {
-      setCurrentUser(user);
-    });
+      setCurrentUser(user)
+    })
+  }, [currentUser])
 
+  useEffect(() => {
     socket.on('chat', (payload) => {
       setChat([...chat, payload])
     })
@@ -45,48 +52,67 @@ function App() {
 
   const GoogleLoginButton = () => {
     const login = () => {
-      const auth = getAuth();
+      const auth = getAuth()
       signInWithPopup(auth, provider)
         .then((result) => {
-          const user = result.user;
+          const user = result.user
           setCurrentUser(user)
-
-        }).catch((error) => {
+        })
+        .catch((error) => {
           console.error(error)
-        });
+        })
     }
-    return <button className='login' onClick={login}>Login Using Google</button>
+    return (
+      <button className="login" onClick={login}>
+        Login Using Google
+      </button>
+    )
   }
 
+  const signOutUser = async () => {
+    try {
+      await signOut(getAuth())
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <>
-      {currentUser == null ? <GoogleLoginButton /> :
-        <div className="App App-header">
-          <div>
-            <h1>Chat app</h1>
-            {chat.map((payload, index) => {
-              return (
-                <p key={index}>
-                  <span>{payload.userName}:</span> {payload.message}
-                </p>
-              )
-            })}
+      {currentUser == null ? (
+        <GoogleLoginButton />
+      ) : (
+        <>
+          <button className="signoutbtn" onClick={signOutUser}>
+            Sign Out
+          </button>
+          <div className="App App-header">
+            <div>
+              <h1>Chat app</h1>
+              {chat.map((payload, index) => {
+                return (
+                  <p key={index}>
+                    <span>{payload.userName}:</span> {payload.message}
+                  </p>
+                )
+              })}
 
-            <form onSubmit={sendChat}>
-              <input
-                type="text"
-                name="chat"
-                placeholder="send text"
-                value={message}
-                onChange={(e) => {
-                  setMessage(e.target.value)
-                }}
-              />
-              <button type="submit">Send</button>
-            </form>
+              <form onSubmit={sendChat}>
+                <input
+                  type="text"
+                  name="chat"
+                  placeholder="send text"
+                  value={message}
+                  onChange={(e) => {
+                    setMessage(e.target.value)
+                  }}
+                />
+                <button type="submit">Send</button>
+              </form>
+            </div>
           </div>
-        </div>}
+        </>
+      )}
     </>
   )
 }
---
+++ readme.md
@@ -0,0 +1,34 @@
+### Tech Stack -
+
+- Frontend - react (18.3.1)
+- Backend - express (4.19.2), nodemon (3.1.4), socket.io (4.7.5)
+
+---
+
+- #### Starting the client
+
+  - Navigate to frontend folder
+  - Run the following command to install all dependencies
+
+    ```bash
+    npm install
+    ```
+
+  - Run the following command to start the react client
+    ```bash
+    npm start
+    ```
+
+- #### Starting the server
+
+  - Navigate to backend folder
+  - Run the following command to install all dependencies
+
+    ```bash
+    npm install
+    ```
+
+  - Run the following command to start the node server
+    ```bash
+    npm start
+    ```
